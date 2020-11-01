2a156d41da8f6831d15e0bf955b781f4ff5a25a9
Marius Erlandsen <marius.s.erlandsen@gmail.com>
2020-11-01 20:05:39 +0100

    some tidying

--- main.go
+++ main.go
@@ -8,33 +8,38 @@ import (
 	"regexp"
 	"sort"
 	"strings"
+	"time"
+)
+
+var (
+	searchURI = "https://www.finn.no/car/used/search.html?location=20061&location=22030&location=22038&make=0.749&model=1.749.2000264&price_from=100000&q=120+fully&sort=0&year_from=2019"
 )
 
 func main() {
-	r, err := http.Get("https://www.finn.no/car/used/search.html?location=20061&location=22030&location=22038&make=0.749&model=1.749.2000264&price_from=100000&q=120+fully&sort=0&year_from=2019")
-	if err != nil {
-		log.Fatal(err)
-	}
+	r, err := http.Get(searchURI)
+	check(err)
+	
+	today := time.Now().Format("2006-01-02")
+	fmt.Println(today)	
 
 	b, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+	check(err)
 
-	re := regexp.MustCompile(`finnkode=[0-9]{9}`)
-	foundBytes := re.FindAll(b, -1)
+	foundBytes := regexp.MustCompile(`finnkode=[0-9]{9}`).FindAll(b, -1)
 
-	finnKoder := []string{}
+	adIDs := []string{}
 	for _, v := range foundBytes {
-		finnKoder = append(finnKoder, string(v))
+		adIDs = append(adIDs, string(v))
 	}
 
-	list := removeDuplicates(finnKoder)
-	trimText(list)
-	sort.Strings(list)
+	adList := removeDuplicates(adIDs)
+	trimText(adList)
+	sort.Strings(adList)
 	
-	for _, kode := range list {
-		fmt.Println(kode + " " + getPrice(kode))
+	listings := map[string](string){}
+	for _, kode := range adList {
+		listings[kode] = getPrice(kode)
+		// fmt.Println(kode + " " + getPrice(kode))
 	}
 	
 }
@@ -89,4 +94,10 @@ func trimWhiteSpaceAndKR(s string) string {
 	s = strings.Replace(s, string('\u00A0'), "", -1) // forbanna irriterende NO-BREAK SPACE
 	s = strings.Replace(s, "kr", "", -1)
 	return s
+}
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
 }
